f5ca6e7f03c3b77bebd9ed62cae27a8db0f935b5
Chakyu23 <yannbourgeron23@gmail.com>
2023-11-13 20:43:59 +0100

    update all

--- Python/Bdd.py
+++ Python/Bdd.py
@@ -1,9 +1,9 @@
 import mysql.connector
-import Config
 
 project_Ares_bdd = mysql.connector.connect(
-    host=Config.connection_params["host"],
-    user=Config.connection_params["user"],
-    password=Config.connection_params["password"],
-    database=Config.connection_params["database"]
+    host= "localhost",
+    user= "Bot Ares",
+    password= "ARES1",
+    database= "project_ares"
 )
+
--- Python/Config.py
+++ Python/Config.py
@@ -2,10 +2,5 @@ Config = {
     "Command_Prefix" : "$",
 }
 
-connection_params = {
-    'host': "localhost",
-    'user': "Bot Ares",
-    'password': "ARES1",
-    'database': "project_ares",
-}
+
 
